9cd16c979d04b27915c0c49afa878e6cc9a39cc7
salahadell <66200658+salahadell@users.noreply.github.com>
2021-01-07 05:02:31 +0200

    Update Loss_Functions.py

--- Loss_Functions.py
+++ Loss_Functions.py
@@ -4,25 +4,30 @@ class Loss_Functions():
         self.loss_type=loss_type
         #self.pred
         #self.labels
+        
     def forward(self,pred,labels):
-        m = labels.shape[1]
+        self.m=labels.shape[1]
         self.pred=pred
         self.labels=labels
+        
         if self.loss_type=="LOG":
-            return np.sum(-np.log(np.absolute((labels/2)-0.5+pred)))/m
+            return (1/self.m)*np.sum(-np.log(np.absolute((labels/2)-0.5+pred)))
+        
         elif self.loss_type=="MEAN":
-            return np.sum(np.power(pred-labels,2))/(2*m)
+            return np.sum(np.power(pred-labels,2))/(2*self.m)
+        
         elif self.loss_type=="CROSSENTROPY":
-            return -np.sum((np.multiply(labels,np.log(pred))+np.multiply((1-labels),np.log(1-pred))))/m
+            return (-1/self.m)*np.sum(np.multiply(labels,np.log(pred))+np.multiply(1-labels,np.log(1-pred)))
 
     def backward(self):
-        m = self.labels.shape[1]
         if self.loss_type=="LOG":
-            return (1/m)*(((-1/self.pred)*((1+self.labels)/2)) + ((1/(1-self.pred))*((1-self.labels)/2)))
+            return (1/self.m)*(((-1/self.pred)*((1+self.labels)/2)) + ((1/(1-self.pred))*((1-self.labels)/2)))
+        
         elif self.loss_type=="MEAN":
-            return (1/m)*(self.pred-self.labels)
+            return (1/self.m)*(self.pred-self.labels)
+        
         elif self.loss_type=="CROSSENTROPY":
-            return (1/m)*((-self.labels/self.pred)+(1-self.labels/1-self.pred))
+            return (1/self.m)*((-self.labels/self.pred)+(1-self.labels/1-self.pred))
         
         
         
