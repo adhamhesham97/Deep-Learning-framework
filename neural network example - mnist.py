5dc4c4c24b709cf5fbe3bf991a85913b8cbb7299
salahadell <66200658+salahadell@users.noreply.github.com>
2021-01-14 10:34:04 +0200

    Update neural network example - mnist.py

--- neural network example - mnist.py
+++ neural network example - mnist.py
@@ -12,7 +12,6 @@ batch_size = 25
 num_epochs = 10
 num_classes = 10
 hidden_units = 300
-Lambda=0
 
 num_of_features = Features_Train.shape[0]
 num_of_examples = Features_Train.shape[1]
@@ -25,9 +24,10 @@ model.add(num_of_features, hidden_units, 'Relu')
 model.add(hidden_units, num_classes, 'Linear')
 optim = DL.optimizer(0.001)
 loss_fn = DL.loss_Function('SoftmaxCrossEntropy')
+loss_fn.setLambda(0)
 
 model.fit(Features_Train, Label_Train_hotone,
-          batch_size, num_epochs, Lambda, optim, loss_fn)
+          batch_size, num_epochs, optim, loss_fn)
 
 
 # test on the same trained data set
