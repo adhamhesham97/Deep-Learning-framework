61e7b8a3a7180c5447617108060b7322acc924f3
AmrElGamal <43656772+AmrElGamal@users.noreply.github.com>
2021-01-18 01:24:59 +0200

    Update Optimizer.py

--- modules/Optimizer.py
+++ modules/Optimizer.py
@@ -1,20 +1,30 @@
+import numpy as np
 class optimizer:
     
-    def __init__(self,learningRate=0.1):
+     
+    def __init__(self,learningRate=0.1,beta=0):
         self.lr=learningRate
+     
+   
+    def step(self,layers,beta):
         
-        
-    def step(self,layers):
-        
+        v={}
+            
         for layer in layers:
             
             w,b=layer.getParams()
             dw,db=layer.getGrads()
             
+            v["dW"+str(layer)]=np.zeros(dw.shape)
+            v["db"+str(layer)]=np.zeros(db.shape)
+            
+            v["dW"+str(layer)]=beta*v["dW"+str(layer)]+(1-beta)*dw
+            v["db"+str(layer)]=beta*v["db"+str(layer)]+(1-beta)*db
+            
             #updating the layer parameters 
-            w=w-self.lr*dw
-            b=b-self.lr*db
+            w=w-self.lr*v["dW"+str(layer)]
+            b=b-self.lr*v["db"+str(layer)]
+            
             
             #storing the new parameters in layer
             layer.setParams(w,b)
-            
