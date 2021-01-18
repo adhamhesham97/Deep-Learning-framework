ac86ec9ef3d6926b4003ee53c8423a586bb410af
AmrElGamal <43656772+AmrElGamal@users.noreply.github.com>
2021-01-18 13:47:01 +0200

    Update Optimizer.py

--- modules/Optimizer.py
+++ modules/Optimizer.py
@@ -4,27 +4,30 @@ class optimizer:
      
     def __init__(self,learningRate=0.1,beta=0):
         self.lr=learningRate
-     
+        self.beta=beta
+        
+        
+        
    
     def step(self,layers):
-        beta=0
+        
         v={}
-            
+        i=0
         for layer in layers:
             
             w,b=layer.getParams()
             dw,db=layer.getGrads()
-            
-            v["dW"+str(layer)]=np.zeros(dw.shape)
-            v["db"+str(layer)]=np.zeros(db.shape)
-            
-            v["dW"+str(layer)]=beta*v["dW"+str(layer)]+(1-beta)*dw
-            v["db"+str(layer)]=beta*v["db"+str(layer)]+(1-beta)*db
+            v["dW"+str(i+1)]=np.zeros(dw.shape)
+            v["db"+str(i+1)]=np.zeros(db.shape)
+           
+            v["dW"+str(i+1)]=self.beta*v["dW"+str(i+1)]+(1-self.beta)*dw
+            v["db"+str(i+1)]=self.beta*v["db"+str(i+1)]+(1-self.beta)*db
             
             #updating the layer parameters 
-            w=w-self.lr*v["dW"+str(layer)]
-            b=b-self.lr*v["db"+str(layer)]
-            
+            w=w-self.lr*v["dW"+str(i+1)]
+            b=b-self.lr*v["db"+str(i+1)]
+            i=i+1
             
             #storing the new parameters in layer
             layer.setParams(w,b)
+            
