3ec65a05a082e0519536cd13e7b10d1f2fec284d
Adham Hesham <adhamhesham1997@gmail.com>
2021-01-18 11:16:16 +0200

    Convolution and pool layers added

---
+++ modules/conv_layer.py
@@ -0,0 +1,191 @@
+from .Activation import activation
+import numpy as np
+
+class conv_layer():
+    def __init__(self, activation_type="Linear", Filter=2, num_of_filters=5, input_dimensions=(1,1,1), Stride=1, padding=0):
+        
+        self.act_func = activation(activation_type)
+        self.S = Stride
+        self.P = padding
+        
+        try:
+            FH, FW = Filter    # Filter is tuple (3,4)
+        except:
+            FH = Filter   # Filter is an integer [square filter]
+            FW = Filter 
+        
+        # input_dimensions is tuple (32, 32, 3)
+        n_H_prev, n_W_prev, n_C_prev = input_dimensions 
+        
+        # layer parameters
+        # filters dimensions are (h,w,c,n)
+        self.filters = np.random.randn(FH, FW, n_C_prev, num_of_filters) * 0.01
+        self.b = np.zeros((1,1,1,num_of_filters))
+        
+        # the dimensions of output
+        self.n_H = int(((n_H_prev+2*self.P-FH)/self.S)+1)
+        self.n_W = int(((n_W_prev+2*self.P-FW)/self.S)+1)
+        self.n_C = num_of_filters
+        
+    def zero_pad(self, X, P):
+        return np.pad(X, ((0,0), (P,P),(P,P), (0,0)), mode='constant', constant_values = (0,0))
+        
+    def conv_single_step(self, X_slice_prev, W, b):
+        s = np.multiply(X_slice_prev, W)
+        # Sum over all entries of the volume s.
+        Z = np.sum(s)
+        # Add bias b to Z. Cast b to a float() so that Z results in a scalar value.
+        Z+=float(b) 
+        return Z
+    
+    def forward(self, X):
+        # X dimensions are (m,h,w,c)
+        X_pad = self.zero_pad(X, self.P)
+        
+        # number of examples
+        m, _,_,_ = X.shape
+        
+        # filter dimensions
+        FH, FW, _,_ = self.filters.shape
+        
+        # output array
+        Z = np.zeros((m, self.n_H, self.n_W, self.n_C))
+        
+        for i in range(m):                      # loop over the batch of training examples
+            X_prev_pad = X_pad[i,:,:,:]         # Select ith training example's padded activation
+            for h in range(self.n_H):           # loop over vertical axis of the output volume
+                # Find the vertical start and end of the current "slice" (≈2 lines)
+                vert_start = h * self.S
+                vert_end = vert_start + FH
+                for w in range(self.n_W):       # loop over horizontal axis of the output volume
+                    # Find the horizontal start and end of the current "slice" (≈2 lines)
+                    horiz_start = w * self.S
+                    horiz_end = horiz_start + FW
+                    for c in range(self.n_C):   # loop over channels (= #filters) of the output volume
+                        # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                        X_slice_prev = X_prev_pad[vert_start:vert_end,horiz_start:horiz_end,:]
+                        # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈3 line)
+                        Z[i, h, w, c] = self.conv_single_step(X_slice_prev, self.filters[:,:,:,c], self.b[:,:,:,c])
+                        
+        A = self.act_func.forward(Z)
+        assert(A.shape == (m, self.n_H, self.n_W, self.n_C))
+        self.X = X # cache input for backprop
+        return A
+
+    def backward(self, dA):
+        # number of examples
+        m, _,_,_ = dA.shape
+        
+        # output arrays
+        dX = np.zeros(self.X.shape)                           
+        dfilters = np.zeros(self.filters.shape)
+        db = np.zeros(self.b.shape)
+        
+        X_pad = self.zero_pad(self.X, self.P)
+        dX_pad = self.zero_pad(dX, self.P)
+        
+        # filter dimensions
+        FH, FW, _,_ = self.filters.shape
+        
+        # back propagate from activation function
+        dA = self.act_func.backward(dA) 
+        
+        # back propagate from convolution
+        for i in range(m):                       # loop over the training examples
+        
+            # select ith training example from A_prev_pad and dA_prev_pad
+            x_pad = X_pad[i]
+            dx_pad = dX_pad[i]
+            
+            for h in range(self.n_H):                   # loop over vertical axis of the output volume
+                for w in range(self.n_W):               # loop over horizontal axis of the output volume
+                    for c in range(self.n_C):           # loop over the channels of the output volume
+                        
+                        # Find the corners of the current "slice"
+                        vert_start = h*self.S
+                        vert_end = vert_start + FH
+                        horiz_start = w*self.S
+                        horiz_end = horiz_start + FW
+                        
+                        # Use the corners to define the slice from a_prev_pad
+                        x_slice = x_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+    
+                        # Update gradients for the window and the filter's parameters using the code formulas given above
+                        dx_pad[vert_start:vert_end, horiz_start:horiz_end, :] += self.filters[:,:,:,c] * dA[i, h, w, c]
+                        dfilters[:,:,:,c] += x_slice * dA[i, h, w, c]
+                        db[:,:,:,c] += dA[i, h, w, c]
+                        
+            
+            # Set the ith training example's dA_prev to the unpaded da_prev_pad (Hint: use X[pad:-pad, pad:-pad, :])
+            pad=self.P
+            dX[i, :, :, :] = dx_pad[pad:-pad, pad:-pad, :]
+        
+        
+        self.dfilters=dfilters
+        self.db=db
+        
+        return dX
+
+    
+    def getParams(self):
+        return self.w, self.b
+
+    def getGrads(self):
+        return self.dfilters, self.db    
+
+    def setParams(self, filters, b):
+        self.filters = filters
+        self.b = b
+        
+    def getLayerParams(self):
+        return self.filters, self.b, self.S, self.P, self.n_H, self.n_W, self.n_C, self.act_func.activation_type
+
+    def setLayerParams(self, LayerParams):
+        self.filters, self.b, self.S, self.P, self.n_H, self.n_W, self.n_C, activation_type = LayerParams
+        self.act_func = activation(activation_type)
+
+
+# forward and backward propagation
+'''
+activation_type="Linear"
+Filter = 2
+num_of_filters = 8
+input_dimensions = (4, 4, 3)
+Stride = 2 
+padding = 2
+conv = conv_layer(activation_type, Filter, num_of_filters, input_dimensions, Stride, padding)
+
+np.random.seed(1)
+X = np.random.randn(10,4,4,3)
+filters = np.random.randn(2,2,3,8)
+b = np.random.randn(1,1,1,8)
+conv.setParams(filters, b)
+
+A = conv.forward(X)
+print("A's mean =", np.mean(A))
+print("A[3,2,1] =", A[3,2,1])
+
+dX=conv.backward(A)
+dfilters, db = conv.getGrads()
+print("dX_mean =", np.mean(dX))
+print("dfilters_mean =", np.mean(dfilters))
+print("db_mean =", np.mean(db))
+'''
+
+# storing and loading
+
+'''
+par = conv.getLayerParams()
+
+conv2=conv_layer()
+conv2.setLayerParams(par)
+A = conv2.forward(X)
+print("\n\n\nA's mean =", np.mean(A))
+print("A[3,2,1] =", A[3,2,1])
+
+dX=conv2.backward(A)
+dfilters, db = conv2.getGrads()
+print("dX_mean =", np.mean(dX))
+print("dfilters_mean =", np.mean(dfilters))
+print("db_mean =", np.mean(db))
+'''
---
+++ modules/pool_layer.py
@@ -0,0 +1,154 @@
+import numpy as np
+
+class pool_layer():
+    def __init__(self, Filter=2, input_dimensions=(1,1,1), Stride=1, mode = "max"):
+        
+        self.mode = mode # maxpool (max) or average pool (average)
+        self.S = Stride
+        
+        try:
+            FH, FW = Filter    # Filter is tuple (3,4)
+        except:
+            FH = Filter   # Filter is an integer [square filter]
+            FW = Filter 
+       
+        self.FH = FH
+        self.FW = FW
+        
+        # input_dimensions is tuple (32, 32, 3)
+        n_H_prev, n_W_prev, n_C_prev = input_dimensions 
+        
+        # the dimensions of output
+        self.n_H = int(((n_H_prev-FH)/self.S)+1)
+        self.n_W = int(((n_W_prev-FW)/self.S)+1)
+        self.n_C = n_C_prev
+        
+        
+    def forward(self, X):    
+        # X dimensions are (m,h,w,c)
+        # number of examples
+        m, _,_,_ = X.shape
+        
+        # output array
+        A = np.zeros([m, self.n_H, self.n_W, self.n_C])
+        
+        for i in range(m):                      # loop over the batch of training examples
+            for h in range(self.n_H):           # loop over vertical axis of the output volume
+                for w in range(self.n_W):       # loop over horizontal axis of the output volume
+                    for c in range(self.n_C):   # loop over channels (= #filters) of the output volume
+                   
+                        # Find the corners of the current "slice"
+                        vert_start = h * self.S
+                        vert_end = vert_start + self.FH
+                        horiz_start = w * self.S
+                        horiz_end = horiz_start + self.FW
+                        
+                        # Use the corners to define the current slice on the ith training example of A_prev, channel c. (≈1 line)
+                        a_prev_slice = X[i, vert_start:vert_end, horiz_start:horiz_end, c]
+                        
+                        # Compute the pooling operation on the slice. Use an if statment to differentiate the modes. Use np.max/np.mean.
+                        if self.mode == "max":
+                            A[i, h, w, c] = np.max(a_prev_slice)
+                            
+                        elif self.mode == "average":
+                            A[i, h, w, c] = np.mean(a_prev_slice)    
+        
+        self.X = X # cache input for backprop
+        return A
+    
+    def backward(self, dA):
+        # number of examples
+        m, _,_,_ = dA.shape
+        
+        dX = np.zeros(self.X.shape)
+        
+        for i in range(m):                       # loop over the training examples                                        
+            x = self.X[i]                        # select training example from A_prev 
+            for h in range(self.n_H):                 # loop on the vertical axis
+                for w in range(self.n_W):             # loop on the horizontal axis
+                    for c in range(self.n_C):         # loop over the channels (depth)
+                        # Find the corners of the current "slice" 
+                        vert_start = h * self.S
+                        vert_end = vert_start + self.FH
+                        horiz_start = w * self.S
+                        horiz_end = horiz_start + self.FW
+                        
+                        # Compute the backward propagation in both modes.
+                        if self.mode == "max":
+                            # Use the corners and "c" to define the current slice from a_prev 
+                            x_slice = x[vert_start:vert_end, horiz_start:horiz_end, c]
+                            # Create the mask from a_prev_slice 
+                            mask = (x_slice == np.max(x_slice))
+                            # Set dA_prev to be dA_prev + (the mask multiplied by the correct entry of dA) 
+                            dX[i, vert_start:vert_end, horiz_start:horiz_end, c] += np.multiply(mask, dA[i, h, w, c])
+                        elif self.mode == "average":
+                            # Get the value a from dA 
+                            da = dA[i,h,w,c]
+                            # Distribute it to get the correct slice of dA_prev. i.e. Add the distributed value of da. 
+                            dX[i, vert_start: vert_end, horiz_start: horiz_end, c] += da/(self.FW*self.FH)
+                            
+        return dX
+   
+    def getLayerParams(self):
+        return self.S, self.n_H, self.n_W, self.n_C, self.FH, self.FW, self.mode
+
+    def setLayerParams(self, LayerParams):
+        self.S, self.n_H, self.n_W, self.n_C, self.FH, self.FW, self.mode = LayerParams
+
+
+# forward propagation
+'''
+F = 3
+S = 2
+input_dims = (4, 4, 3)
+maxpool = pool_layer(F, input_dims, S)
+avgpool = pool_layer(F, input_dims, S, 'average')
+
+np.random.seed(1)
+X = np.random.randn(2, 4, 4, 3)
+print("maxpool forward:\n",maxpool.forward(X))
+print("avgpool forward:\n",avgpool.forward(X))
+'''
+
+# back propagation
+'''
+F = 2
+S = 1
+input_dims = (5, 3, 2)
+maxpool = pool_layer(F, input_dims, S)
+avgpool = pool_layer(F, input_dims, S, 'average')
+
+np.random.seed(1)
+X = np.random.randn(5, 5, 3, 2)
+A = maxpool.forward(X)
+A = avgpool.forward(X)
+dA = np.random.randn(5, 4, 2, 2)
+
+
+dX = maxpool.backward(dA)
+print("mode = max")
+print('mean of dA = ', np.mean(dA))
+print('dX[1,1] = ', dX[1,1])  
+print()
+dX = avgpool.backward(dA)
+print("mode = average")
+print('mean of dA = ', np.mean(dA))
+print('dX[1,1] = ', dX[1,1])
+'''
+
+# storing and loading
+
+'''
+par = maxpool.getLayerParams()
+
+maxpool2 = pool_layer()
+maxpool2.setLayerParams(par)
+A = maxpool2.forward(X)
+
+dX = maxpool2.backward(dA)
+print("mode = max")
+print('mean of dA = ', np.mean(dA))
+print('dX[1,1] = ', dX[1,1])  
+print()
+'''
+
